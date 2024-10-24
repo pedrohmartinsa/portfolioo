f753db04334462f5f921836e2e6e3cfd802c8005
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-10-24 19:08:00 -0300

    a

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,6 +9,9 @@ import Header from "./Components/Header"
 
 function App() {
 
+
+  'h-[80%] w-[100%] overflow-auto md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-3'
+
   return (
     <>
       <div className='text-primary bg-secondary h-full'>
@@ -16,10 +19,11 @@ function App() {
             <div className="flex flex-col justify-between border-primary border-2 w-[95vw] h-screen p-5">
               <Header/>
               <div className="flex flex-col justify-between gap-4 items-center md:grid md:grid-cols-[100px_auto_auto] md:grid-rows-2 h-[85%]">
-                <div className="h-[80%] w-[100%] overflow-auto md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-3">
+              
+                <div className="w-[100%] h-[80%] md:absolute md:bottom-0 md:right-0 md:w-[75vw] overflow-y-scroll md:h-[70%] md:pl-0 md:p-7 md:mr-4">
                   <Outlet/>
                 </div>
-                <div className="mb-2 md:col-start-1 md:row-start-1">
+                <div className="md:col-start-1 md:row-start-1">
                   <Nav/>
                 </div>
               </div>
