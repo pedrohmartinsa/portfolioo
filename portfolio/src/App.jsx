cb3e5ee8042d3f0f037eb839d4ab8d37dff06629
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-10-24 19:55:45 -0300

    a

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,14 +12,14 @@ function App() {
     <>
       <div className='text-primary bg-secondary md:h-screen'>
           <div className="flex flex-col items-center justify-center pt-8 pb-3 h-[100%]">
-            <div className="flex flex-col justify-between border-primary border-2 w-[95vw] h-screen p-5">
+            <div className="flex flex-col gap-7 justify-between border-primary border-2 w-[95vw] h-screen p-5">
               <Header/>
-              <div className="flex flex-col justify-between gap-4 items-center md:grid md:grid-cols-[100px_auto_auto] md:grid-rows-2 h-[85%] md:h-[70%]">
+              <div className="flex flex-col justify-between items-center md:grid md:grid-cols-[100px_auto_auto] md:grid-rows-2 h-[80%] md:h-[70%]">
               
-                <div className="md:mb-10 h-[90%] md:absolute md:bottom-0 md:right-0 md:w-[75vw] overflow-y-scroll md:h-[80%] md:pl-0 md:p-7 md:mr-16">
+                <div className="md:mb-10 md:absolute md:bottom-0 md:right-0 md:w-[75vw] overflow-y-scroll md:h-[80%] md:pl-0 md:p-7 md:mr-16">
                   <Outlet/>
                 </div>
-                <div className="md:col-start-1 md:row-start-1">
+                <div className="mt-7 md:col-start-1 md:row-start-1">
                   <Nav/>
                 </div>
               </div>
