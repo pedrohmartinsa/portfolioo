f61ed45ea9f1c3018f4c85e2886eabc9f07d9f9c
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-10-24 19:09:00 -0300

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
               <Header/>
               <div className="flex flex-col justify-between gap-4 items-center md:grid md:grid-cols-[100px_auto_auto] md:grid-rows-2 h-[85%]">
               
-                <div className="w-[100%] h-[80%] md:absolute md:bottom-0 md:right-0 md:w-[75vw] overflow-y-scroll md:h-[70%] md:pl-0 md:p-7 md:mr-4">
+                <div className="w-[100%] h-[70%] md:absolute md:bottom-0 md:right-0 md:w-[75vw] overflow-y-scroll md:h-[70%] md:pl-0 md:p-7 md:mr-4">
                   <Outlet/>
                 </div>
                 <div className="md:col-start-1 md:row-start-1">
