a29537ba1fd2b204b1c90c43ae0773f2693bf19a
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-11-12 20:24:17 -0300

    Update Home.jsx

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home() {
     return(
         <>
-            <div className="font-bold p-4 h-auto md:h-0">
+            <div className="flex justify-center items-center font-bold p-4 h-auto md:h-full">
                 <div>
-                    <p>Olá, me chamo Pedro Henrique Martins Alves dos Santos, nascido e criado em São Paulo, sou um jovem muito alegre explorando e me apaixonando pelo mundo da tecnologia e programação. E aqui neste portfólio você verá um pouco mais sobre mim, minhas habilidades e projetos durante essa jornada pelo mundo tecnológico.</p>
+                    <p className="md:w-96">Olá, me chamo Pedro Henrique Martins Alves dos Santos, nascido e criado em São Paulo, sou um jovem muito alegre explorando e me apaixonando pelo mundo da tecnologia e programação. E aqui neste portfólio você verá um pouco mais sobre mim, minhas habilidades e projetos durante essa jornada pelo mundo tecnológico.</p>
                 </div>
             </div>
         </>
